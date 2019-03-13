0f1d88febaae3472d9c94a96f453dc06677c25b9
Vados <Vados@DESKTOP-FF9CVKD>
2019-03-13 21:57:35 +0200

    add my work

---
+++ src/ua/com/qalight/Entity/Currency.java
@@ -0,0 +1,31 @@
+package ua.com.qalight.Entity;
+
+public enum Currency {
+
+	USD("$", "USD", 840),
+	EUR("€", "EUR", 978),
+	RUB("₽", "RUB", 643);
+	
+	private final String symbol;
+	private final String shortName;
+	private final Integer code;
+	
+	Currency(String symbol, String shortName, Integer code) {
+		this.symbol = symbol;
+		this.shortName = shortName;
+		this.code = code;
+	}
+
+	public String getSymbol() {
+		return symbol;
+	}
+
+	public String getShortName() {
+		return shortName;
+	}
+
+	public Integer getCode() {
+		return code;
+	}
+
+}
---
+++ src/ua/com/qalight/service/FileManager.java
@@ -0,0 +1,65 @@
+package ua.com.qalight.service;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Map;
+import ua.com.qalight.Entity.CurrencyEntity;
+
+public class FileManager {
+	
+	private static final String INPUT_FILE_PATH = 
+			System.getProperty("user.dir") + 
+			System.getProperty("file.separator") + 
+			"files" + 
+			System.getProperty("file.separator") + 
+			"InputFile.txt";
+	
+	private static final String OUTPUT_FILE_PATH = 
+			System.getProperty("user.dir") + 
+			System.getProperty("file.separator") + 
+			"files" + 
+			System.getProperty("file.separator") + 
+			"OutputFile.txt";
+	
+	
+	
+
+	public static Map<String, Double> readInputCurrencyVaues(){
+		
+		CurrencyEntity currencyEntity = new CurrencyEntity();
+		
+		
+		try(FileInputStream fileInputStream = new FileInputStream(INPUT_FILE_PATH);
+			ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream); 
+		){
+		 
+			currencyEntity = (CurrencyEntity) objectInputStream.readObject();
+			
+		}catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+		
+		
+		return (Map<String, Double>).currencyEntity  ;
+	}
+	
+	public static void writeCurrencyValuesToFile(CurrencyEntity currency, Map<String, Double> currencyMap) {
+		
+		try(
+				FileOutputStream fileOutputStream = new FileOutputStream(OUTPUT_FILE_PATH);
+				ObjectOutputStream objOutputStream = new ObjectOutputStream(fileOutputStream); 
+			){
+			 
+			objOutputStream.writeObject(currency);
+			objOutputStream.writeObject(currencyMap);	
+			
+			}catch (Exception e) {
+				
+				e.printStackTrace();
+			}
+		
+	}
+}
